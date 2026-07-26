df4d46cc4895a2b62b67777a68818a623d5b4419
Pranavamurthy K S <pranavamurthyks45@gmail.com>
2026-07-26 08:15:55 +0530

    temp commit

--- src/mira2_actions/src/bt_runner.cpp
+++ src/mira2_actions/src/bt_runner.cpp
@@ -1,13 +1,27 @@
 #include "rclcpp/rclcpp.hpp"
+#include "geometry_msgs/msg/pose_stamped.h"
+
+
 
 class BTRunner : public rclcpp::Node
 {
+    public:
     BTRunner() : rclcpp::Node("bt_runner")
-    {}
+    {
+        orangeFlareSubscriber = this->create_subscription<geometry_msgs::msg::PoseStamped>("/orange_flare/pose", 10, callback_function);
+    }
 
-    // Subscriber to orangle flare pose 
+    void flare_callback(const geometry_msgs::msg::PoseStamped::SharedPtr msg)
+    {
+        
+    }
+
 
     // Change the variable for isflarevisible 
+
+    private:
+    rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr orangeFlareSubscriber;
+
 
 
 };
